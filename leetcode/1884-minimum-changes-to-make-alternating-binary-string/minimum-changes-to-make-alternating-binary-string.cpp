55f823c0dc606d5098262eb2ee57b9fad82f3938
Rbj <31772878+rahulbijoor@users.noreply.github.com>
2023-12-24 14:12:36 -0800

    Time: 6 ms (35.63%) | Memory: 7.4 MB (24.25%) - LeetSync

---
+++ leetcode/1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int c1=0;//string starting with 0
+        int c2=0;// string staring with 1
+        for(int i=0;i<s.length();i++)
+        {
+            if(i%2==0){
+                if(s[i]=='0'){
+                    c2++;
+                }
+                else{
+                    c1++;
+                }
+            }
+            else if(i%2!=0){
+                if(s[i]=='0'){
+                    c1++;
+                }
+                else{
+                    c2++;
+                }
+            }
+        }
+        return min(c1,c2);
+    }
+};
