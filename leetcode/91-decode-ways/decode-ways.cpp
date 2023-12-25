0607d12a0fcabfa84c9c53fa0534f06498e961ab
Rbj <31772878+rahulbijoor@users.noreply.github.com>
2023-12-25 15:51:00 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.56%) - LeetSync

---
+++ leetcode/91-decode-ways/decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(int index,string s,vector<int> &memo){
+        if(index==s.length()){
+            return 1;
+        }
+        if(s[index]=='0'){
+            return 0;
+        }
+        if(memo[index]!=-1){
+            return memo[index];
+        }
+        memo[index]=numDecodings(index+1,s,memo);
+        if(index+1<s.length() && (s[index]=='1' || (s[index]=='2' && s[index+1]<='6'))){
+            memo[index]+=numDecodings(index+2,s,memo);
+        }
+        return memo[index];
+    }
+    int numDecodings(string s) {
+        vector<int> memo(s.length(),-1);
+        return numDecodings(0,s,memo);
+    }
+};
