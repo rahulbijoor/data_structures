fdec6048f09b9b3e31a1bfa57ca28bc33fbd0a9b
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2023-12-31 17:28:04 -0800

    Time: 1627 ms (5.02%) | Memory: 17.8 MB (75.56%) - LeetSync

---
+++ leetcode/455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int count=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        for(int i = 0 ; i < g.size() ; i++ ){
+            for(int j=0;j<s.size();j++){
+                if( s[j] >= g[i] ){
+                    count++;
+                    s[j]=-1;
+                    break;
+                }
+            }
+
+        }
+        return count;
+    }
+};
