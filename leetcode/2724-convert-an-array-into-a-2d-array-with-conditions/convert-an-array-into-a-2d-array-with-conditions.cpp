08b20d0db71000d41b0c993135007557e6924669
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-01-01 18:48:31 -0800

    Time: 54 ms (5.35%) | Memory: 30.4 MB (26.85%) - LeetSync

---
+++ leetcode/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        unordered_map<int,int>umap;
+        int num=0;
+        for(int i=0;i<nums.size();i++){
+            if(umap.find(nums[i])!=umap.end()){
+                umap[nums[i]]++;
+            }
+            else{
+                umap[nums[i]]=1;
+            }
+            num=max(umap[nums[i]],num);
+        }
+        vector<vector<int>> result;
+        for(int i=0;i<num;i++){
+            vector<int> row;
+            for(auto& item : umap){
+                if(item.second!=0){
+                    cout<<item.first<<" : "<<item.second<<endl;
+                    row.push_back(item.first);
+                    item.second--;
+                }
+                
+               
+
+            }
+            result.push_back(row);
+        }
+        return result;
+    }
+};
