828bdc97e10e04ff7681124ef88bb7dc173d1f7e
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-01-17 20:26:20 -0800

    Time: 0 ms (100.00%) | Memory: 6.3 MB (71.72%) - LeetSync

--- leetcode/70-climbing-stairs/climbing-stairs.cpp
+++ leetcode/70-climbing-stairs/climbing-stairs.cpp
@@ -11,7 +11,17 @@ public:
         return memo[n];
     }
     int climbStairs(int n){
-        vector<int> memo(n+1,-1);
-        return climbStairs(n,memo);
+        int dp[n+1];
+        if(n<=2){
+            return n;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
