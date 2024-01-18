bbeb8392159d525a764bd7b2a005a67211cfe09c
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-01-17 20:26:38 -0800

    Time: 2 ms (45.07%) | Memory: 6.3 MB (88.87%) - LeetSync

--- leetcode/70-climbing-stairs/climbing-stairs.cpp
+++ leetcode/70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    int climbStairs(int n,vector<int> &memo) {
-        if(n<=2){
-            return n;
-        }
-        if(memo[n]!=-1){
-            return memo[n];
-        }
-        memo[n]=climbStairs(n-1,memo)+climbStairs(n-2,memo);
-        return memo[n];
-    }
+    
     int climbStairs(int n){
         int dp[n+1];
         if(n<=2){
