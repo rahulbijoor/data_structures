f6563a2570951e1a919bc685694b7e7dd7866f3e
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-04-28 18:29:10 -0700

    Time: 177 ms (12.92%) | Memory: 91.4 MB (25.13%) - LeetSync

---
+++ leetcode/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int ans=0;
+        for(int i=0;i<31;i++){
+            int mask=1<<i;
+            int r=k&mask;
+            int c=0;
+            for(int x: nums){
+                if(mask&x){
+                    c++;
+                }
+            }
+            if(r != 0 && c%2 == 0){
+                ans++;
+            }
+            else if( r == 0 && c%2 != 0){
+                ans++;
+            }
+            
+        }
+        return ans;
+    }
+};
