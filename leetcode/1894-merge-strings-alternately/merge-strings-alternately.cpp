cc070f347abf4d576c6b64bc28db3eb2c2d5a5ba
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-05-19 09:49:31 -0700

    Time: 0 ms (100.00%) | Memory: 7.7 MB (44.77%) - LeetSync

--- leetcode/1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ leetcode/1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,25 +2,14 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string word3="";
-        int l1=word1.length();
-        int l2=word2.length();
-        int l=0;
-        int i=0;
-        int j=0;
-        while(i < l1 && j < l2){
-            if(l%2==0){
-            word3+=word1[i++];
+        int maxLength=max(word1.length(),word2.length());
+        for(int i = 0; i< maxLength; i++) {
+            if(i < word1.length()){
+                word3+=word1[i];
             }
-            else{
-                word3+=word2[j++];
+            if(i < word2.length()){
+                word3+=word2[i];
             }
-            l++;
-        }
-        while(i<l1){
-            word3+=word1[i++];
-        }
-        while(j<l2){
-            word3+=word2[j++];
         }
         return word3;
     }
