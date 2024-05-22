fef0b18c2b5f733f03785a3c50b2f4afc126b2ae
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-05-22 11:41:17 -0700

    Time: 8 ms (43.28%) | Memory: 9.8 MB (25.32%) - LeetSync

--- leetcode/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ leetcode/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        vector<char> vowels;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-                vowels.push_back(s[i]);
+        string ch="";
+        for( int i = s.length() - 1 ; i >= 0 ; i-- ){
+            if( s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+                ch+=s[i];
             }
         }
-        int ind=vowels.size()-1;
+        int ind=0;
         string result="";
-        for(int i=0;i<s.length();i++){
-             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
-                result+=vowels[ind--];
-             }
-             else{
-                result+=s[i];
-             }
+        for( int j = 0; j < s.length(); j++ ){
+            if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'){
+                result+=ch[ind++];
+            }
+            else{
+                result+=s[j];
+            }
         }
         return result;
     }
