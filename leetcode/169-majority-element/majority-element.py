f381387a6628d9aba9d178349945a7ebc7dbc7f4
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-05-31 16:34:56 -0700

    Time: 153 ms (85.34%) | Memory: 13.4 MB (24.07%) - LeetSync

--- leetcode/169-majority-element/majority-element.py
+++ leetcode/169-majority-element/majority-element.py
@@ -4,14 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        hashtable={}
-        for num in nums:
-            if num in hashtable.keys():
-                hashtable[num]=hashtable[num]+1
-            else :
-                hashtable[num]=1
-            if hashtable[num] > len(nums)/2:
-                return num
-        return 0
+        nums.sort()
+        return nums[len(nums)/2]
             
         
