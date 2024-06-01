14a8c9428a0dabff9606127d5b86273ec5361cd6
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-05-31 20:47:02 -0700

    Time: 12 ms (84.35%) | Memory: 11.4 MB (98.47%) - LeetSync

---
+++ leetcode/3379-score-of-a-string/score-of-a-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum=0
+        if len(s) <= 1:
+            return 0
+        for i in range(1,len(s)):
+            sum += abs(ord(s[i])-ord(s[i-1]))
+        return sum
+
+        
