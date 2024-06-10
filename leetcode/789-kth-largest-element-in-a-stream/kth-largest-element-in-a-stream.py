0c99a9c65c487e651e76c2663b07d5fc0d8ac73d
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-06-09 19:39:43 -0700

    Time: 95 ms (32.45%) | Memory: 15.8 MB (99.23%) - LeetSync

--- leetcode/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ leetcode/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,41 +1,30 @@
 class KthLargest(object):
-    
+
     def __init__(self, k, nums):
         """
         :type k: int
         :type nums: List[int]
         """
-        self.minheap=nums
+        self.nums=nums
         self.k = k
-        heapq.heapify(self.minheap)
-        while len(self.minheap) > k:
-            heapq.heappop(self.minheap)
+        heapq.heapify(nums)
+        while len(nums) > k :
+            heapq.heappop(nums)
 
-        
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
-        heapq.heappush(self.minheap,val)
-        if len(self.minheap) > self.k:
-            heapq.heappop(self.minheap)
-        return self.minheap[0]
+        heapq.heappush(self.nums,val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
+
         
 
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
-
-#Understand
-#
-#
-#
-#Match
-
-#Pseudocode
-# sort(nums) 
-# return nums[k-1]
-
