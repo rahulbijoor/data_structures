739c6d3de9087fff594e4551ccf5c29300e2e2a1
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-06-14 07:53:09 -0700

    Time: 49 ms (5.24%) | Memory: 11.4 MB (99.80%) - LeetSync

---
+++ leetcode/13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,57 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum=0
+        i=0
+        while i < len(s): 
+            if s[i] == 'M':
+                sum += 1000
+            elif s[i] == 'D':
+                sum += 500
+            elif s[i] == 'C':
+                if i+1 < len(s):
+                    if s[i+1] == 'D':
+                        sum += 400
+                        i=i+1
+                    elif s[i+1] == 'M':
+                        sum += 900
+                        i=i+1
+                    else:
+                        sum += 100
+                else:
+                    sum += 100
+            elif s[i] == 'L':
+                sum += 50
+            elif s[i] == 'X':
+                if i+1 < len(s):
+                    if s[i+1] == 'L':
+                        sum += 40
+                        i+=1
+                    elif s[i+1] == 'C':
+                        sum += 90
+                        i+=1
+                    else:
+                        sum += 10
+                else:
+                    sum += 10
+            elif s[i] == 'V':
+                sum += 5
+            elif s[i] == 'I':
+                if i+1 < len(s):
+                    if s[i+1] == 'V':
+                        sum += 4
+                        i+=1
+                    elif s[i+1] == 'X':
+                        sum += 9
+                        i+=1
+                    else:
+                        sum += 1
+                else:
+                    sum += 1
+            i+=1
+            print(sum)
+        return sum
+            
