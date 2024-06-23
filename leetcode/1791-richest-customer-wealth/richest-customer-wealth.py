28664ad9297f4d7938f53f8558f9870d3c1c99a8
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-06-22 17:30:50 -0700

    Time: 64 ms (5.87%) | Memory: 16.8 MB (16.23%) - LeetSync

---
+++ leetcode/1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,32 @@
+class Solution:
+    #Understand
+    #Space/ Time ?
+    #Empty Grid?
+    #Negative numbers?
+    #Do all the customers have same number of bank accounts?
+    #Match
+    #Array
+    #Without Array
+
+    #Plan
+    #Approach 1
+    # create an array of size number of rows in the grid
+    # find the sum of each row and append it to the array
+    # find the max value in the array
+
+    #Approach two
+    # variable max_sum
+    # calculate sum of each row
+    # if the sum is greater than max_sum then max_sum=sum
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_sum=0
+        for i in range(0,len(accounts)):
+            row_sum=0
+            for j in range(0,len(accounts[i])):
+                print(accounts[i][j],end =" ")
+                row_sum += accounts[i][j]
+            print()
+            if max_sum < row_sum:
+                max_sum=row_sum
+            
+        return max_sum    
