1336decaa25ad00490a35c20f9e5e1a02d7d660c
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-07-14 21:49:44 -0700

    Time: 337 ms (67.99%) | Memory: 38.3 MB (56.25%) - LeetSync

--- leetcode/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ leetcode/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -5,13 +5,25 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        nodes=[]
-        curr=head
-        while curr:
-            nodes.append(curr.val)
-            curr=curr.next
         result=0
-        for i in range(len(nodes)//2):
-            result=max(result,nodes[i]+nodes[len(nodes)-1-i])
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow= slow.next
         
+        nextNode=slow
+        prev=None
+        while slow:
+            nextNode = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nextNode
+        result=0
+        curr=head
+        while prev:
+            result=max(result,curr.val+prev.val)
+            prev=prev.next
+            curr=curr.next
+            
         return result
