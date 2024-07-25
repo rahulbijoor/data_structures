07563165877aa27db2f377601b4bb66a35244e3c
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-07-24 18:26:47 -0700

    Time: 1201 ms (27.39%) | Memory: 24 MB (68.76%) - LeetSync

--- leetcode/948-sort-an-array/sort-an-array.py
+++ leetcode/948-sort-an-array/sort-an-array.py
@@ -1,40 +1,40 @@
 class Solution:
+    def mergeSort(self, nums: List[int], low: int, high: int):
+        if low < high:
+            mid = low + (high - low) // 2
+            self.mergeSort(nums, low, mid)
+            self.mergeSort(nums, mid + 1, high)
+            self.merge(nums, low, mid, high)
 
-    def sortArray(self, nums: List[int]) -> List[int]:
-        
-        def mergeSort(nums: List[int], low: int, high: int):
-            if low < high:
-                mid = low + (high - low) // 2
-                mergeSort(nums, low, mid)
-                mergeSort(nums, mid + 1, high)
-                merge(nums, low, mid, high)
+    def merge(self, nums: List[int], low: int, mid: int, high: int):
+        leftSubArray = nums[low:mid + 1]
+        rightSubArray = nums[mid + 1:high + 1]
 
-        def merge(nums: List[int], low: int, mid: int, high: int):
-            leftSubArray = nums[low:mid + 1]
-            rightSubArray = nums[mid + 1:high + 1]
+        i = 0
+        j = 0
+        k = low
 
-            i = 0
-            j = 0
-            k = low
-
-            while i < len(leftSubArray) and j < len(rightSubArray):
-                if leftSubArray[i] <= rightSubArray[j]:
-                    nums[k] = leftSubArray[i]
-                    i += 1
-                else:
-                    nums[k] = rightSubArray[j]
-                    j += 1
-                k += 1
-
-            while i < len(leftSubArray):
+        while i < len(leftSubArray) and j < len(rightSubArray):
+            if leftSubArray[i] <= rightSubArray[j]:
                 nums[k] = leftSubArray[i]
                 i += 1
-                k += 1
-
-            while j < len(rightSubArray):
+            else:
                 nums[k] = rightSubArray[j]
                 j += 1
-                k += 1
+            k += 1
+
+        while i < len(leftSubArray):
+            nums[k] = leftSubArray[i]
+            i += 1
+            k += 1
+
+        while j < len(rightSubArray):
+            nums[k] = rightSubArray[j]
+            j += 1
+            k += 1
+    def sortArray(self, nums: List[int]) -> List[int]:
         
-        mergeSort(nums, 0, len(nums) - 1)
+       
+        
+        self.mergeSort(nums, 0, len(nums) - 1)
         return nums
