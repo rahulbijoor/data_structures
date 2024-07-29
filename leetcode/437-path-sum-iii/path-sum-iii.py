d4329e747b9c92f2d5a96de79b3c47099e35025e
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-07-28 18:14:12 -0700

    Time: 392 ms (12.66%) | Memory: 16.9 MB (81.59%) - LeetSync

--- leetcode/437-path-sum-iii/path-sum-iii.py
+++ leetcode/437-path-sum-iii/path-sum-iii.py
@@ -5,24 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+    def countPath(self, root: Optional[TreeNode], targetSum: int, count: List[int]):
         if not root:
-            return 0
+            return
         
-        def dfs(node: Optional[TreeNode], targetSum: int) -> int:
-            if not node:
-                return 0
-
-            paths=helper(node, targetSum)
-            leftpath=dfs(node.left, targetSum) 
-            rightpath=dfs(node.right, targetSum)
-            return leftpath + rightpath + paths
         
-        def helper(node: Optional[TreeNode], target: int) -> int:
-            if not node:
-                return 0
-            rec=0
-            if (target - node.val) == 0:
-                rec=1
-            return helper(node.left, target-node.val) + helper(node.right, target-node.val) + rec
-        return dfs(root,targetSum)
+        
+        if targetSum - root.val == 0:
+            count[0] += 1
+        self.countPath(root.left, targetSum - root.val, count)
+        self.countPath(root.right, targetSum - root.val, count)
+    def dfs(self, root: Optional[TreeNode], targetSum: int,count: List[int]):
+        if not root:
+            return
+        
+        self.dfs(root.left, targetSum, count)
+        self.countPath(root, targetSum, count)
+        self.dfs(root.right, targetSum, count)
+        
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        count = [0]
+        self.dfs(root,targetSum,count)
+        return count[0]
+        
