7005a0905469727b1c8cf2901621af859e0f3f11
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-08-12 21:34:54 -0700

    Time: 46 ms (51.39%) | Memory: 19.5 MB (62.22%) - LeetSync

--- leetcode/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ leetcode/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,34 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self, node: Optional[TreeNode], k: List[int]) -> int:
-        if not node:
-            return None
-        
-        left = self.helper(node.left,k)
-        if left:
-            return left
-        k[0] -= 1
-        if k[0] == 0:
-            return node.val
-        right = self.helper(node.right,k)
-        if right:
-            return right
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        result=0
-        def helper(node: Optional[TreeNode]):
-            nonlocal k
-            nonlocal result
-
-            if not node or k < 0:
+        result=[]
+        def Inorder(node,k):
+            if not node:
                 return
-
-            helper(node.left)
-            k -= 1
-            if k == 0:
-                result = node.val
-                return
-            helper(node.right)
-        helper(root)
-        return result
+            
+            Inorder(node.left,k-1)
+            result.append(node.val)
+            Inorder(node.right,k-1)
+        
+        Inorder(root,k)
+        return result[k-1]
         
