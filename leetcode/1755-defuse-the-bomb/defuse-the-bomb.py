51b02b83b60d47c6228e4f63ab0c1f09429721d7
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-11-25 09:11:18 -0800

    Time: 11 ms (9.93%) | Memory: 16.9 MB (13.82%) - LeetSync

--- leetcode/1755-defuse-the-bomb/defuse-the-bomb.py
+++ leetcode/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,19 +1,14 @@
-from typing import List
-
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        n = len(code)
-        if k == 0:
-            return [0] * n
         
-        res = [0] * n
-        start, end = (1, k) if k > 0 else (k, -1)
-        window_sum = sum(code[i % n] for i in range(start, end + 1))
-        
-        for i in range(n):
-            res[i] = window_sum
-
-            window_sum -= code[(i + start) % n]
-            window_sum += code[(i + end + 1) % n]
+        res = []
+        l = len(code)
+        for i in range(l):
+            if k == 0:
+                res.append(0)
+            elif k > 0:
+                res.append(sum(code[(i+j)%l] for j in range(1,k+1)))
+            else:
+                res.append(sum(code[(i+j)%l] for j in range(k,0)))
         
         return res
