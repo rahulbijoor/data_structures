287f6a18343739d644c446050cae62175d7d1b60
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-12-28 15:44:25 -0800

    Time: 22 ms (25.55%) | Memory: 13.8 MB (5.97%) - LeetSync

--- leetcode/724-find-pivot-index/find-pivot-index.py
+++ leetcode/724-find-pivot-index/find-pivot-index.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        total = 0
         l = len(nums)
         leftSum =[0]*l
         rightSum = [0]*l
         for i in range(1,l):
             leftSum[i] = leftSum[i-1] + nums[i-1]
-        
         for i in range(l-2,-1,-1):
             rightSum[i] = rightSum[i+1] + nums[i+1]
-        
+            
         for i in range(l):
             if leftSum[i] == rightSum[i]:
                 return i 
+        
         return -1
