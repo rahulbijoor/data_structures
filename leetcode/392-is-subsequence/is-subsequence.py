5118a7948e23a19f2454f9402c62f628965c22b8
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-12-28 05:24:38 -0800

    Time: 4 ms (17.43%) | Memory: 12.4 MB (18.31%) - LeetSync

--- leetcode/392-is-subsequence/is-subsequence.py
+++ leetcode/392-is-subsequence/is-subsequence.py
@@ -5,11 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        l1=0
-        l2=0
-        while l1<len(s) and l2<len(t):
-            if s[l1] == t[l2]:
-                l1 += 1
-            l2 += 1
-        print(l1,l2)
-        return l1 == len(s)        
+        i = 0
+        j = 0
+        l1 = len(s)
+        l2 = len(t)
+
+        if l1 > l2:
+            return False
+        
+        while j < l2:
+            if i == l1:
+                return True
+
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        if i == l1:
+            return True
+
+        
+        return False
+
+
+        
