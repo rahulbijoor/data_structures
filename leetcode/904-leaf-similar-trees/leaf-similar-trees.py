be7dc46f0fd165236cd64382befe87385207bfec
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-12-29 04:07:28 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.73%) - LeetSync

--- leetcode/904-leaf-similar-trees/leaf-similar-trees.py
+++ leetcode/904-leaf-similar-trees/leaf-similar-trees.py
@@ -1,25 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def leafSequence(self,node: Optional[TreeNode],leaf_Seq: List[int]):
-        if not node:
-            return  
-        if not node.left and not node.right:
-            leaf_Seq.append(node.val)
-            return
-        
-        self.leafSequence(node.left,leaf_Seq)
-        
-        self.leafSequence(node.right,leaf_Seq)
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: bool
+        """
+        list1 = []
+        list2 = []
+        def helper(node,list):
+            if not node:
+                return
+            if not node.left and not node.right:
+                list.append(node.val)
+                return
+            
+            helper(node.left,list)
+            helper(node.right,list)
 
-    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        leaf1=[]
-        leaf2=[]
-        self.leafSequence(root1,leaf1)
-        self.leafSequence(root2,leaf2)
-
-        return leaf1 == leaf2
+        helper(root1,list1)
+        helper(root2,list2)
+        if len(list1) != len(list2):
+            return False
+        
+        for i in range(len(list1)):
+            if list1[i] != list2[i]:
+                return False
+        return True
+        
