c7680f3fb1a475a45e10eca717ae1887b99d8e73
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-12-29 07:14:30 -0800

    Time: 47 ms (64.54%) | Memory: 20.9 MB (5.19%) - LeetSync

--- leetcode/1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ leetcode/1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -1,36 +1,32 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def maxLevelSum(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
         if not root:
             return 0
-
-        queue = [root]
-        level = 1
-        maxLevel = 1
+        
         maxSum = root.val
+        queue = deque([root])
+        level = 0
+        maxLevel = 1
         
         while queue:
-            level_length = len(queue)
-            current_sum = 0
-            
-            for _ in range(level_length):
-                node = queue.pop(0)
-                current_sum += node.val
-                
+            level += 1
+            levelSum = 0
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                levelSum += node.val
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
             
-            if current_sum > maxSum:
-                maxSum = current_sum
+            # Update maxSum and maxLevel only if levelSum is strictly greater
+            if levelSum > maxSum:
+                maxSum = levelSum
                 maxLevel = level
-            
-            level += 1
         
         return maxLevel
+        
