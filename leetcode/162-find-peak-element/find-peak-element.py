583d0fc1e550f38762a4e468ce89a10e01b59760
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-12-29 20:23:06 -0800

    Time: 3 ms (7.62%) | Memory: 12.7 MB (8.11%) - LeetSync

--- leetcode/162-find-peak-element/find-peak-element.py
+++ leetcode/162-find-peak-element/find-peak-element.py
@@ -1,22 +1,17 @@
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return 0
-        if nums[0] > nums[1]:
-            return 0
-        if nums[n-1] > nums[n-2]:
-            return n-1
-        l = 1
-        h = n-2
-        while l <= h:
-            m = l+(h-l)//2
-            nums_length = len(nums)
-            if  nums[(m - 1) % nums_length] < nums[m % nums_length] > nums[(m + 1) % nums_length]:
-                return m
-            elif nums[(m + 1) % nums_length] > nums[m % nums_length]:
-                l = (m+1)
-            elif nums[(m - 1) % nums_length] > nums[m % nums_length]:
-                h = (m-1)
-            
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prev = float('-inf')
+        for i in range(len(nums)):
+            next  = 0
+            if i+1 < len(nums):
+                next = nums[i+1]
+            else:
+                next = float('-inf')
+            if prev < nums[i] > next:
+                return i
+
         
