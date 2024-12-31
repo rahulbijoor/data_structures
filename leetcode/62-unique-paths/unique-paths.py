05ae65f99eedfefafb8f9f5ad4e7832412796229
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2024-12-30 23:35:26 -0800

    Time: 29 ms (5.22%) | Memory: 12.6 MB (6.99%) - LeetSync

--- leetcode/62-unique-paths/unique-paths.py
+++ leetcode/62-unique-paths/unique-paths.py
@@ -7,6 +7,15 @@ class Solution(object):
         """
         dp =[[-1]*n for i in range(m)]
         print(dp)
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+        print(dp)
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] =dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
         def helper(i,j,dp):
             
             if i == m-1 and j == n-1:
