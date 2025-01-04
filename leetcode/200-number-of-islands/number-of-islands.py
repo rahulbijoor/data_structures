97d99b426292e67f7f06ded3b7c1a77225d4ff8c
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-01-03 21:12:45 -0800

    Time: 236 ms (73.71%) | Memory: 20.3 MB (44.18%) - LeetSync

--- leetcode/200-number-of-islands/number-of-islands.py
+++ leetcode/200-number-of-islands/number-of-islands.py
@@ -1,27 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
-        print(visited)
-        def bfs(row: int,col: int):
-            q = collections.deque()
-            q.append((row,col))
-            visited[row][col] = True
-
-            while q:
-                row, col = q.popleft()
-                moves=[(row-1, col), (row+1, col), (row, col-1), (row, col+1)]
-                for new_row, new_col in moves:
-                    if new_row >= 0 and new_row < len(grid) and new_col >= 0 and new_col < len(grid[0]) and grid[new_row][new_col] == "1" and not visited[new_row][new_col]:
-                        visited[new_row][new_col] = True
-                        q.append((new_row,new_col))
-                    
+        rows,cols = len(grid), len(grid[0])
         count = 0
-        for row in range(len(grid)):
-
-            for col in range(len(grid[0])):
-
-                if not visited[row][col] and grid[row][col] == "1":
-                    bfs(row,col)
+        def dfs(r,c):
+            if r < 0 or c < 0 or r == rows or c == cols or grid[r][c] == '0':
+                return False
+            
+            grid[r][c] ='0'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            return True
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and dfs(r,c):
                     count += 1
-        print(visited)
         return count
+
