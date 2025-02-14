d31f7b9c3a2d465ff2abe4afee6d15b1acb03e7c
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-02-14 06:50:50 -0800

    Time: 10 ms (7.18%) | Memory: 17.8 MB (84.69%) - LeetSync

--- leetcode/139-word-break/word-break.py
+++ leetcode/139-word-break/word-break.py
@@ -4,9 +4,11 @@ class Solution:
         dp = [False]*(l+1)
         dp[0] = True
         for i in range(1,l+1):
-            for j in range(0,i):
-                if dp[j] and s[j:i] in wordDict:
+            for w in wordDict:
+                l2 = len(w)
+                if i >= l2 and dp[i-l2] and s[i-l2:i] in wordDict:
                     dp[i] = True
                     break
+            
         return dp[l]
                     
