9f44b5e996d2e42359abc3257d33ec5228ea985b
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-03-04 19:04:52 -0800

    Time: 148 ms (23.08%) | Memory: 15.7 MB (7.69%) - LeetSync

--- leetcode/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ leetcode/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -4,16 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = []
-        dp.append(0)
-        dp.append(1)
-        dp.append(5)
-        if n <= 2:
-            return dp[n]
-        curr = 4
-        for i in range(3,n+1):
-            dp.append(dp[i-1]+curr+4)
-            curr += 4
+        dp = 1
+        if n <= 1:
+            return n
+        for i in range(2,n+1):
+            dp += (4*(i-1))
         
-        return dp[-1]
+        return dp
         
