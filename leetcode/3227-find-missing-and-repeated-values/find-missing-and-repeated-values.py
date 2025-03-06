6829b7e37ad1567c43bf0326e8fef994f02623d8
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-03-06 12:38:32 -0800

    Time: 5 ms (78.69%) | Memory: 18.4 MB (23.22%) - LeetSync

--- leetcode/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ leetcode/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -2,14 +2,14 @@ class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
         totalSum = ((n**2)*((n**2)+1))//2
-        ht = {}
+        ht = set()
         res = []
         Sum = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 Sum += grid[r][c]
-                if grid[r][c] not in ht.keys():
-                    ht[grid[r][c]] = 1
+                if grid[r][c] not in ht:
+                    ht.add(grid[r][c])
                 else:
                     res.append(grid[r][c])
         missing = totalSum - (Sum-res[0]) 
