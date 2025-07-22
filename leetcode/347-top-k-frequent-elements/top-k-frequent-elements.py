22e8acefb2c08d7212ddd7adf45252eae4b890bf
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-07-22 11:43:40 -0700

    Time: 8 ms (73.55%) | Memory: 14.6 MB (34.54%) - LeetSync

--- leetcode/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ leetcode/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -8,4 +8,6 @@ class Solution(object):
         fq = Counter(nums)
         r = sorted(fq.items(),key=lambda x: x[1], reverse=True)
         top_k = [item[0] for item in r[:k]]
+        lst = [("a", 3), ("b", 1),("bc",2), ("c", 2)]
+        print(sorted(lst, key=lambda x: len(x[0]),reverse = True))
         return top_k
