f92d471cdd7f191306bab3bf52bab77591f1305b
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-07-23 14:51:03 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.22%) - LeetSync

--- leetcode/198-house-robber/house-robber.py
+++ leetcode/198-house-robber/house-robber.py
@@ -4,13 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) < 2:
-            return nums[0]
-        dp = [0] * len(nums)
+        i = 0
+        dp = [-1]*(len(nums)+1)
         dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
+        
+        n = len(nums)
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[1],nums[0])
+        dp[1] = max(nums[1],nums[0])
         for i in range(2,len(nums)):
-            dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+            dp[i] = max(nums[i] + dp[i-2],dp[i-1])
         
-        return dp[-1]
-        
+        return dp[n-1]
