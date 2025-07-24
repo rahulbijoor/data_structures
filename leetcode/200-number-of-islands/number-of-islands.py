090d3d9e73af7b00527b428f3f313fa7f1b6912e
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-07-24 16:24:10 -0700

    Time: 227 ms (93.37%) | Memory: 20.1 MB (82.39%) - LeetSync

--- leetcode/200-number-of-islands/number-of-islands.py
+++ leetcode/200-number-of-islands/number-of-islands.py
@@ -1,25 +1,22 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        def dfs(grid,row,col):
-            if row <0 or col< 0 or row>= len(grid) or col >= len(grid[0]) or grid[row][col] =='0':
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        def dfs(row, col):
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or grid[row][col] == "0":
                 return
-            grid[row][col] ='0'
-            dfs(grid,row+1,col)
-            dfs(grid,row,col+1)
-            dfs(grid,row-1,col)
-            dfs(grid,row,col-1)
             
+            grid[row][col] = "0"
+            dfs(row+1, col)
+            dfs(row-1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
         count = 0
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
+                if grid[i][j] == "1":
+                    dfs(i,j)
                     count += 1
-                    dfs(grid,i,j)
         return count
-
-
-        
