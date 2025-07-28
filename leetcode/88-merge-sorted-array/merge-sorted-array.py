9225ad63162844108182ed855c3f21eebc8bb2d7
Rahul Bijoor <31772878+rahulbijoor@users.noreply.github.com>
2025-07-28 11:29:21 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (43.40%) - LeetSync

--- leetcode/88-merge-sorted-array/merge-sorted-array.py
+++ leetcode/88-merge-sorted-array/merge-sorted-array.py
@@ -1,34 +1,27 @@
-class Solution(object):
-    # Understand
-    # Time / Space Complexity?
-    # Empty Lists?
-    # Non Decreasing order?
-
-    # Match
-    # Two sperarate pointers
-
-    # Plan
-    # traverse through the arrays
-    # if array1[ele1] < array2[ele1]  array1 will be same ele1 += 1
-    # else insert array2[ele2] into array1 at index ele1 ele2 += 1
-
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        index1 = m - 1
-        index2 = n-1
-        insertIndex = m + n - 1
-        while index2 >= 0:
-            if index1 >= 0 and nums1[index1] > nums2[index2]:
-                nums1[insertIndex] = nums1[index1]
-                index1 -= 1
+        i = m-1
+        j = n-1
+        k = len(nums1)-1
+        if n == 0:
+            return nums1
+        while i > -1 and j > -1:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                nums1[insertIndex] = nums2[index2]
-                index2 -= 1
-            insertIndex -= 1
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        
+        while j > -1:
+            nums1[k] = nums2[j]
+            k-=1
+            j-=1
         return nums1
+
+
+            
